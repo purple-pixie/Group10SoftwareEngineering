78f8bac73e897be7e11bf5411b2f0700aab36a2d
Michael <Michael@10.0.9.178>
2012-03-09 10:55:45 +0000

    test 2 - new line added

--- src/AntGame.java
+++ src/AntGame.java
@@ -4,6 +4,7 @@ public class AntGame {
 	public static void main(String args[]) {
 		System.out.println("Hello world!");
 		System.out.println("Hello world 2!");
+		System.out.println("New GITHUB test");
 	}
 
 }
