f9f99f95cd9fffb29586e15f14f46b329b7b1846
MisterGreen <dups@me.com>
2012-03-15 17:45:45 +0000

    Change on site test.

--- src/AntGame.java
+++ src/AntGame.java
@@ -1,10 +1,10 @@
-
 public class AntGame {
 	
 	public static void main(String args[]) {
 		System.out.println("Hello world!");
 		System.out.println("New GITHUB test");
-		System.out.println("FOOBAR");
+		System.out.println("FOO!");
+		System.out.println("FOOBAR2000");
 
 	}
 
