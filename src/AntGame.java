e5b458e25d0e862d50ecc7663ddf032f5da3af24
James Green <dups@me.com>
2012-03-15 17:40:09 +0000

    Yet more testing

--- src/AntGame.java
+++ src/AntGame.java
@@ -3,8 +3,9 @@ public class AntGame {
 	
 	public static void main(String args[]) {
 		System.out.println("Hello world!");
-		System.out.println("Hello world 2!");
 		System.out.println("New GITHUB test");
+		System.out.println("FOOBAR");
+
 	}
 
 }
